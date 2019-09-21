52e0885ae9768b369466afb8beb8d64e13947f52
mihaiconstantin <c.tinmihai@ymail.com>
2019-09-21 15:07:47 +0200

    Added slide layout with one column

---
+++ content/presentation/slides/layouts/OneColumn.vue
@@ -0,0 +1,39 @@
+<template>
+    <section class="one-column-layout template">
+        <div class="canvas shadow-md">
+            <!-- Pins. -->
+            <pins></pins>
+
+            <!-- Title. -->
+            <h3 class="title ribbon ribbon-left">{{ title }}</h3>
+
+            <!-- Content. -->
+            <div class="content">
+                <slot>Wide content here.</slot>
+            </div>
+        </div>
+    </section>
+</template>
+
+
+
+<script>
+    module.exports = {
+        name: 'one-column-layout',
+
+        components: {
+            'pins': httpVueLoader('../decorations/Pins.vue'),
+        },
+
+        props: {
+            title: {
+                default: "One-column layout",
+                type: String
+            }
+        }
+    }
+</script>
+
+
+
+<style scoped></style>
