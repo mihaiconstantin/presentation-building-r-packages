d530a48e0eb99ba744003ffeb98856836e44f8e7
mihaiconstantin <c.tinmihai@ymail.com>
2019-09-21 23:17:11 +0200

    Added `R.vue` slide for tools section

--- content/presentation/slides/subslides/tools/R.vue
+++ content/presentation/slides/subslides/tools/R.vue
@@ -1,5 +1,17 @@
 <template>
-    <layout :title="title"></layout>
+    <layout :title="title">
+        <!-- Left content. -->
+        <div slot="left" class="slot-content-left">
+            <ul>
+                <li v-for="(item, index) in items" :key="index">{{ item }}</li>
+            </ul>
+        </div>
+
+        <!-- Right content. -->
+        <div slot="right" class="slot-content-right">
+            <img :src="logo" class="shadow-sm tool-logo" alt="R lagnauge logo">
+        </div>
+    </layout>
 </template>
 
 
@@ -14,7 +26,15 @@
 
         data: () => {
             return {
-                title: "`R` language for scripting"
+                title: "`R` language for scripting",
+
+                logo: "content/presentation/assets/img/r_logo.jpg",
+
+                items: [
+                    "implement your methdology",
+                    "run your simulations",
+                    "etc. etc."
+                ]
             }
         }
     }
@@ -22,4 +42,15 @@
 
 
 
-<style scoped></style>
+<style scoped>
+    /* Force different alignment from template. */
+    .content-right {
+        align-items: center;
+        justify-content: center;
+    }
+
+    .slot-content-right .tool-logo {
+        width: 150px;
+        margin: 0;
+    }
+</style>
